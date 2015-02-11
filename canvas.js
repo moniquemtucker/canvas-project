22ca0f7ea71babe086bdd949cfa9d4c1ecfc60ea
moniquemtucker <moniquetucker@gmail.com>
2015-02-11 07:28:01 -0800

    adding animation

--- canvas.js
+++ canvas.js
@@ -1,36 +1,115 @@
 
-// create opening sign
+// create opening welcoming sign using canvas
 var welcome = document.getElementById("welcomeCanvas");
-var ctx = welcome.getContext("2d");
-ctx.font = "70px Lato";
-ctx.textAlign = "center"; 
-ctx.fillText("Welcome to Portland!",welcome.width/2, welcome.height/2);
+var welctx = welcome.getContext("2d");
+welctx.font = "70px Lato";
+welctx.textAlign = "center"; 
+welctx.fillText("Welcome to Portland!", welcome.width/2, welcome.height/2);
 
-// Create 2D circle 
-var c = document.getElementById("ballCanvas");
-var ctx = c.getContext("2d");
+// on page load, hide Oregon state image
+function hidePortland() {
+	document.getElementById("portlandContainer").style.display = "none";
+}
 
-// Create gradient
-var grd = ctx.createRadialGradient(75,50,5,90,60,100);
-grd.addColorStop(0,"green");
-grd.addColorStop(1,"white");
+// initial position of green balls
+var context;
+var x=100;
+var y=200;
+var dx=5;
+var dy=8;
 
-// Fill with gradient
-ctx.arc(50, 50, 20, 0, 2*Math.PI);
-ctx.fillStyle = grd;
-ctx.fill();
+// hide welcoming sign and initialize "Portland experience"
+function init()
+{
+  document.getElementById("welcomeContainer").style.display = "none";
+  document.getElementById("portlandContainer").style.display = "block";
+  context= portlandCanvas.getContext('2d');
 
-function hideImage() {
-	document.getElementById("oregon").style.display = "none";
+// create a stop point for balls bouncing (source: CodeShip)
+function interval(func, wait, times){
+    var interv = function(w, t){
+        return function(){
+            if(typeof t === "undefined" || t-- > 0){
+                setTimeout(interv, w);
+                try{
+                    func.call(null);
+                }
+                catch(e){
+                    t = 0;
+                    throw e.toString();
+                }
+            }
+        };
+    }(wait, times);
+
+    setTimeout(interv, wait);
+};
+
+// draw bouncing balls and then use stop function
+interval(draw,10, 200);
+
+// set delay to clear canvas and draw heart image
+setTimeout(function(){
+  	context.clearRect(0,0, 800,800);
+  	document.getElementById("heartContainer").innerHTML="<img src='heart.png' width=200 height=200 />";
+  	}, 5000);
 }
 
 
-// draw map of Oregon
-function portlandCanvas() {
-	document.getElementById("welcomeContainer").style.display = "none";
-    var c = document.getElementById("portlandCanvas");
-    var ctx = c.getContext("2d");
-    var img = document.getElementById("oregon");
-    ctx.drawImage(img,10,10);
+// create bouncing balls onto canvas
+function draw()
+{	
+  context.clearRect(0,0, 800,800);
+  function ball()
+  {
+    context.beginPath();
+    // create gradient
+    var grd = context.createRadialGradient(x,y,20,x+dx,y+dy,100);
+    grd.addColorStop(0,"green");
+    grd.addColorStop(1,"white");
+
+    // draws a circle of radius 20 at the coordinates x,y on the canvas
+    context.arc(x,y,20,0,Math.PI*2,true);
+
+    // fill balls with gradient
+    context.fillStyle = grd;
+    context.fill();
+    // boundary logic to determine direction of bouncing
+    if( x<0 || x>300) dx = -dx; 
+    if( y<0 || y>300) dy = -dy; 
+    x += dx; 
+    y += dy;
+
+  }
+    ball();
+
+    // draws additional bouncing balls (TO DO: refactor to incorporate DRY principles)
+   function ball2(start, stop, horiz, vert)
+  {
+    context.beginPath();
+    // create gradient
+    var grd = context.createRadialGradient(start,stop,20,start+horiz,stop+vert,100);
+    grd.addColorStop(0,"green");
+    grd.addColorStop(1,"white");
+
+    // draws a circle of radius 20 at the coordinates start,stop on the canvas
+    context.arc(start,stop,20,0,Math.PI*2,true);
+
+    // fill balls with gradient
+    context.fillStyle = grd;
+    context.fill();
+    // boundary logic to determine direction of bouncing
+    if( start<0 || start>300) 
+      horiz = -horiz; 
+    if( stop<0 || stop>300) 
+      vert = -vert; 
+    start += horiz; 
+    stop += vert;
+  }
+  // runs ball() to create additional bouncing balls
+  for (i = 0; i < 4; i++) {
+    ball2(x+100*i, y+30*i, 5, 5);
+  }  
+  ball2(x+200, y, 5,5);
 }
 
